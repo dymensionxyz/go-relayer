8fe61d4ebf72df9845e8eea89501616cd093ec91
danwt <30197399+danwt@users.noreply.github.com>
2024-08-30 16:39:38 +0100

    starting to get into it

--- cmd/tx.go
+++ cmd/tx.go
@@ -805,20 +805,29 @@ $ %s tx connect demo-path --src-port transfer --dst-port transfer --order unorde
 				}
 			}
 
+			_ = srcPort
+			_ = dstPort
+			_ = order
+			_ = version
+
+			return nil
 			// create channel if it isn't already created
-			return c[src].CreateOpenChannels(
-				cmd.Context(),
-				c[dst],
-				retries,
-				to,
-				srcPort,
-				dstPort,
-				order,
-				version,
-				override,
-				memo,
-				pathName,
-			)
+			/*
+				return c[src].CreateOpenChannels(
+					cmd.Context(),
+					c[dst],
+					retries,
+					to,
+					srcPort,
+					dstPort,
+					order,
+					version,
+					override,
+					memo,
+					pathName,
+				)
+
+			*/
 		},
 	}
 	cmd = timeoutFlag(a.viper, cmd)
